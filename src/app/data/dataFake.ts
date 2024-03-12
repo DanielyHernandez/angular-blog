114accbfa18d04447d53bf3f5129ce6db05ef0ee
DanielyHernandez <daniely.ch@hotmail.com>
2024-03-11 21:17:53 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia novo filme do Homem de Ferro, confira",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_T0Zb5ZfW9FtoGa51051VdRJJ2ccDcBsTOw&usqp=CAU"
+
+  },
+
+  {
+    "id":"2",
+    "title": "Nova serie anunciada no Dysney +",
+    "description": "Bla bla bla",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAGIjdXOM4G-2dFdJ_gTbydbAjiBE-S57y5w&usqp=CAU"
+
+  },
+]
